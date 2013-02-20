385bee3a83cfeac7a6106d65cfcc8112a3426ad0
Curtis Quintal <curtisq@gmail.com>
2013-02-20 17:50:49 -0500

    Initial commit
    main tests edge detection on filtered color. function for color
    filterint in filters.cpp

---
+++ openCv setup/Filters.h
@@ -0,0 +1,29 @@
+//
+//  Filters.h
+//  openCv setup
+//
+//  Created by Curtis Quintal on 2/20/13.
+//  Copyright (c) 2013 Curtis Quintal. All rights reserved.
+//
+
+#ifndef openCv_setup_Filters_h
+#define openCv_setup_Filters_h
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<iostream>
+
+using namespace cv;
+using namespace std;
+
+Mat redFilter(const Mat& src);
+Mat blackFilter(const Mat& src);
+Mat whiteFilter(const Mat& src);
+Mat ColorFilter(const Mat& src, int bLow, int gLow, int rLow, int bHigh, int gHigh, int rHigh);
+Mat ColorFilterSrc(const Mat& src, int bLow, int gLow, int rLow, int bHigh, int gHigh, int rHigh);
+
+
+int blackCount(Mat src);
+int whiteCount(Mat src);
+
+#endif
